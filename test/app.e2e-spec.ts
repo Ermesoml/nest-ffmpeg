025a356cfdd6a4a50228f08e4209f7232f89feb1
Ermesom Barros <ermesoml.ufg@gmail.com>
2025-04-03 11:15:32 +0100

    feat: starting the adaptation of e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import * as path from 'path';
+import * as fs from 'fs';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const filePath = path.resolve(__dirname, 'file_example_MP4_1920_18MG.mp4');
+  let fileBuffer: Buffer;
+
+  beforeAll(async () => {
+    try {
+      fileBuffer = fs.readFileSync(filePath);
+    } catch (error) {
+      console.error(`Error reading file: ${filePath}`, error);
+      throw error;
+    }
+  });
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +28,20 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/resize (POST) - Valid resolution and file', async () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/resize')
+      .attach('file', filePath)
+      .field('resolution', '640x480')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Content-Disposition', /attachment; filename="file_example_MP4_1920_18MG.mp4"/);
+  });
+
+  it('/resize (POST) - Invalid resolution format', async () => {
+    return request(app.getHttpServer())
+      .post('/resize')
+      .attach('file', filePath)
+      .field('resolution', 'invalid')
+      .expect(400);
   });
 });
